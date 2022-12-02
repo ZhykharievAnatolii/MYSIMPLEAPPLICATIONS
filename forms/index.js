f8bef5b0f5965d070e73c0d30ab95fb6df2f701f
ZhykharievAnatolii <nekijt87@gmail.com>
2022-12-02 11:10:32 +0200

    upd for js
    new upd

--- forms/index.js
+++ forms/index.js
@@ -16,3 +16,27 @@ form.addEventListener('change', ({ target: input }) => {
   validateInput(input);
 });
 
+// const xhr= new XMLHttpRequest();
+// const list=document.querySelector('ul');
+// xhr.open('GET',url)
+// xhr.send();
+// xhr.response
+// const data=JSON.parse(xhr.response);
+// xhr.onload=()=>{
+//   const data=JSON.parse(xhr.response);
+//   Object.values(data).forEach((transactions)=>{
+//     const item=document.querySelector('li')  ;
+//     item.innerText=transactions.description
+//     list.append(item)
+//   })
+//   // console.log(xhr.response)
+// };
+// console.log(xhr.response);
+// xhr.onerror
+// const url=new URL('http:localhost:3000/users');
+// url.searchParams.append('name','Anatolii');
+// url.href
+// 'http://localhost:3000/users?name=Anatolii'
+// url.searchParams.append('lastname','Anatolii');
+// url.href
+// 'http://localhost:3000/users?name=Anatolii&lastname=Anatolii'
